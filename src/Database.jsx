c200a2e55073a87aaf856c061e0f5896ff8c823e
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2023-10-30 15:55:37 -0500

    updating database page

--- src/Database.jsx
+++ src/Database.jsx
@@ -5,34 +5,34 @@ import { Chart } from "react-google-charts";
 
 const Database = (p) => {
     const props = [{
-        "filename": "src/assets/cat.jpg",
-        "name": "c12h6",
-        "color": "green",
-        "weight": 1.02,
-        "light_absorption": 8.1,
-        "lifespan": 7.2,
-        "bandgap": 3.4,
-        "rank": "2"
+        "filename": "src/assets/C20H12N.png",
+        "name": "c20h12n",
+        "weight": 3.5,
+        "light_absorption": 3.8,
+        "lifespan": 4.3,
+        "bandgap": 7.5,
+        "rank": 3
     },
     {
-        "filename": "src/assets/cat.jpg",
-        "name": "c12h6",
-        "color": "green",
-        "weight": 1.02,
-        "light_absorption": 8.4,
+        "filename": "src/assets/C10H6.png",
+        "name": "c10h6",
+        "weight": 6.8,
+        "light_absorption": 6.7,
+        "lifespan": 3.7,
+        "bandgap": 6.4,
+        "rank": 2
+    },
+    {
+        "filename": "src/assets/C4H3S.png",
+        "name": "c4h3s",
+        "weight": 7.9,
+        "light_absorption": 5.4,
         "lifespan": 7.8,
-        "bandgap": 3.1,
-        "rank": "2"
+        "bandgap": 5.6,
+        "rank": 2
     },
     {
-        "filename": "src/assets/cat.jpg",
-        "name": "c12h6",
-        "color": "green",
-        "weight": 1.02,
-        "light_absorption": 8.5,
-        "lifespan": 7.5,
-        "bandgap": 3.1,
-        "rank": "2"
+        "filename": "src/assets/react.svg"
     },
     {
         "filename": "src/assets/react.svg"
@@ -50,18 +50,21 @@ const Database = (p) => {
     ];
 
     const options = {
-        title: "Molecule Properties",
+        title: "Molecule Properties & Statistics",
         width: 600,
         height: 400,
         bar: { groupWidth: "95%" },
         legend: { position: "none" },
+        hAxis: {
+            minValue: 10,
+          },
     };
 
     return (
         <>
-            <img src={props[0].filename} alt="logo" width={150} height={150} />
-            <img src={props[1].filename} alt="logo" width={150} height={150} />
-            <img src={props[2].filename} alt="logo" width={150} height={150} />
+            <img src={props[0].filename} alt="trimer 1" width={200} height={150} />
+            <img src={props[1].filename} alt="trimer 2" width={175} height={150} />
+            <img src={props[2].filename} alt="trimer 3" width={175} height={150} />
 
             <Chart
                 chartType="BarChart"
@@ -73,7 +76,8 @@ const Database = (p) => {
             <h2>
                 Suggested Molecules
             </h2>
-            <img src={props[3].filename} alt="logo"></img>
+            <img src={props[3].filename} alt="suggested moelcule 1"></img>
+            <img src={props[4].filename} alt="suggested molecule 2"></img>
         </>
     )
 }
