678e994df63521e31e2bb1458d4ed261420e433b
estrella <epopo2@illinois.edu>
2023-10-30 15:47:11 -0500

    add maker page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import PasswordLock from "./PasswordLock";
+import Maker from "./Maker";
 
 function AppRouter() {
     return (
 
         <Routes>
             <Route path="/" element={<PasswordLock />} />
+            <Route path="/maker" element={<Maker />} />
         </Routes>
        
     );
---
+++ src/Maker.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import mmli from './assets/mmli.png'
+import Sidebar from './Sidebar'
+import MainContent from './MainContent'
+import { useState } from 'react'
+
+
+const Maker = () => {
+    const [selectedImages, setSelectedImages] = useState([]);
+  const images = [
+    'src/assets/C4H3N2.png',
+    'src/assets/C4H3S.png',
+    'src/assets/C6H3F.png',
+    'src/assets/C4H3N2.png',
+    'src/assets/C4H3S.png',
+    'src/assets/C6H3F.png',
+    'src/assets/C4H3N2.png',
+    'src/assets/C4H3S.png',
+    'src/assets/C6H3F.png',
+    // Add more image URLs as needed
+  ];
+
+  const handleImageSelect = (index) => {
+    const selectedImage = images[index];
+
+    // Check if the image is already selected and toggle its selection
+    if (selectedImages.includes(selectedImage)) {
+      setSelectedImages(selectedImages.filter((img) => img !== selectedImage));
+    } else {
+      setSelectedImages([...selectedImages, selectedImage]);
+    }
+  };
+
+  return (
+    <section className="maker-container">
+        <img className='mmli-img'src={mmli} alt="mmli" />
+    <div className="main-container">
+      <Sidebar images={images} handleImageSelect={handleImageSelect} selectedImages={selectedImages} />
+      <MainContent selectedImages={selectedImages} />
+    </div>
+    </section>
+  );
+};
+  
+export default Maker
