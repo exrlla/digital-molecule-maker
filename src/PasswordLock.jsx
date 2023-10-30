3fbf307f4018e0982b62f9037ae89f5a1250b26a
mkseay2 <mkseay2@illinois.edu>
2023-10-30 14:16:28 -0500

    display login screen

--- src/PasswordLock.jsx
+++ src/PasswordLock.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import avatar from "./assets/INFO418_login_avatar.png"
+import icons from "./assets/INFO418_login_icons.png"
 
 function PasswordLock() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-        pew pew</h1>
+      <div className='top_right'>
+        <img src={icons} alt="Icons" width={50}/>
+      </div>
+      <div className='center'> 
+        <img src={avatar} alt="Avatar" width={70}/>
+      </div>
     </>
   )
 }
