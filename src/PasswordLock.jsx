ef5e49dd39ee15e5d3b9be9d889a490978838215
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2023-10-30 13:51:08 -0500

    added password logic

--- src/PasswordLock.jsx
+++ src/PasswordLock.jsx
@@ -1,12 +1,34 @@
 import './App.css'
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navigate } from 'react-router-dom';
 
-function PasswordLock() {
-  // const [count, setCount] = useState(0)
+const PasswordLock = () => {
+  const [password, setPassword] = useState("");
+  
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
 
+  const handleSubmit = (e) => {
+    if (password === "75341861") {
+      // go to maker page
+    } else {
+      alert("Wrong password!");
+    }
+    e.preventDefault();
+  }
+  
   return (
     <>
-      <h1>
-        pew pew</h1>
+      <h1>Enter Password</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Password:{" "}
+          <input type="text" value={password} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit"></input>
+      </form>
     </>
   )
 }
