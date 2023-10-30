f5cda8153dd9fd8e6acb5a4ca16dce6e74b132bf
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2023-10-30 15:57:05 -0500

    password lock page

--- src/PasswordLock.jsx
+++ src/PasswordLock.jsx
@@ -1,10 +1,7 @@
 import './App.css'
 import avatar from "./assets/INFO418_login_avatar.png"
 import icons from "./assets/INFO418_login_icons.png"
-<<<<<<< HEAD
 
-=======
->>>>>>> 94c18a54799f6105ffe3085a2779bdc4665a0020
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
@@ -17,12 +14,9 @@ const PasswordLock = () => {
 
   const handleSubmit = (e) => {
     if (password === "7534186168") {
-<<<<<<< HEAD
       // go to maker page
       window.location.replace("https://www.google.com/");
-=======
       Navigate("database")
->>>>>>> 94c18a54799f6105ffe3085a2779bdc4665a0020
     } else {
       alert("Wrong password!");
     }
@@ -36,12 +30,8 @@ const PasswordLock = () => {
       <div className='center'> 
         <img src={avatar} alt="Avatar" width={120}/>
       </div>
-<<<<<<< HEAD
-      <form onSubmit={handleSubmit} className='center'>
-=======
       <h1>Enter Password</h1>
       <form onSubmit={handleSubmit}>
->>>>>>> 94c18a54799f6105ffe3085a2779bdc4665a0020
         <label>
           <input type="text" value={password} onChange={handleChange}>
           </input>
