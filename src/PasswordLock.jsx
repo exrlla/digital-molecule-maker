7a21922fc05372be35a691f2615f06cd853e0728
mkseay2 <mkseay2@illinois.edu>
2023-11-04 00:46:09 -0500

    fixed password lock page (mostly) (avatar and text box are centered at the top of the page for some reason)

--- src/PasswordLock.jsx
+++ src/PasswordLock.jsx
@@ -24,22 +24,19 @@ const PasswordLock = () => {
   
   return (
     <>
-      <div  style={{backgroundImage: `url("/src/assets/INFO418_login_background.webp")`}}>
         <div className='top_right'>
           <img src={icons} alt="Icons" width={60}/>
         </div>
         <div className='center'> 
           <img src={avatar} alt="Avatar" width={120}/>
         </div>
-        <h1>Enter Password</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className='center'>
           <label>
             <input type="text" value={password} onChange={handleChange}>
             </input>
             <button type="submit">Submit</button>
           </label>
         </form>
-      </div>
     </>
   )
 }
