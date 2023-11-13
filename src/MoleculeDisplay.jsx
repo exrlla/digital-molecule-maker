fbe6fe3cd63b770e0071839da6ab3bfd4d8b30d8
estrella <epopo2@illinois.edu>
2023-11-13 13:40:47 -0600

    add proptype validation

--- src/MoleculeDisplay.jsx
+++ src/MoleculeDisplay.jsx
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
 
-const MoleculeDisplay = ({image, width, height}) => {
+const MoleculeDisplay = ({ image, width, height }) => {
     const name = image.substring(11, image.length - 4);
-    console.log(name)
     return (
         <>
-            <div style={{width: width + "px", heigh: height + "px", display: 'flex', flexDirection: 'column', padding: '1rem'}}>
-                <img src={image}/>
+            <div style={{ width: width + "px", height: height + "px", display: 'flex', flexDirection: 'column', padding: '1rem' }}>
+                <img src={image} alt={name} />
                 <p>{name}</p>
             </div>
         </>
     );
 }
+
+MoleculeDisplay.propTypes = {
+    image: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+};
 
 export default MoleculeDisplay;
