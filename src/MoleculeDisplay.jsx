e875e8cfcf7e327f88ed4f0c7ec0e005d38e2156
estrella <epopo2@illinois.edu>
2023-11-13 14:45:04 -0600

    add prop types

--- src/MoleculeDisplay.jsx
+++ src/MoleculeDisplay.jsx
@@ -4,12 +4,19 @@ const MoleculeDisplay = ({image, width, height}) => {
     const name = image.substring(11, image.length - 4);
     return (
         <>
-            <div style={{width: width, heigh: height, display: 'flex', flexDirection: 'column'}}>
+            <div style={{width: width, height: height, display: 'flex', flexDirection: 'column'}}>
                 <img src={image}/>
-                <p style={{marginTop: "2px"}}>{name}</p>
+                <p style={{marginTop: "2px", color:'black'}}>{name}</p>
             </div>
         </>
     );
-}
+};
+
+MoleculeDisplay.propTypes = {
+    image: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+};
+
 
 export default MoleculeDisplay;
