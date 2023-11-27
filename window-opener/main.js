706fa6a9088606b993e85674c8b57d1c9b218844
estrella <epopo2@illinois.edu>
2023-11-27 17:02:33 -0600

    add double monitor code

--- window-opener/main.js
+++ window-opener/main.js
@@ -1,8 +1,5 @@
 const { app, BrowserWindow, screen } = require('electron');
 
-let mainWindow;
-let databaseWindow;
-
 function createWindow(url, screenIndex) {
   const displays = screen.getAllDisplays();
 
@@ -12,13 +9,14 @@ function createWindow(url, screenIndex) {
     return;
   }
 
-  const mainScreen = displays[screenIndex || 0];
+  const currScreen = displays[screenIndex];
 
-  const { width, height } = mainScreen.workAreaSize;
+  const { width, height } = currScreen.workAreaSize;
 
   const window = new BrowserWindow({
-    x: mainScreen.bounds.x,
-    y: mainScreen.bounds.y,
+    fullscreen: true,
+    x: currScreen.bounds.x,
+    y: currScreen.bounds.y,
     width,
     height,
   });
@@ -38,14 +36,8 @@ app.whenReady().then(() => {
   const { width, height } = primaryDisplay.workAreaSize
 
   mainWindow = createWindow('http://localhost:5173/', 0);
-  databaseWindow = createWindow('http://localhost:5173/database', 0);
+  databaseWindow = createWindow('http://localhost:5173/database', 1);
 
-  // app.on('activate', () => {
-  //   if (BrowserWindow.getAllWindows().length === 0) {
-  //     mainWindow = createWindow('http://localhost:5173/', 0);
-  //     databaseWindow = createWindow('http://localhost:5173/database', 1);
-  //   }
-  // });
 });
 
 app.on('window-all-closed', () => {
@@ -53,38 +45,3 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
-
-// NOKDSKJFSDKLJ:FSDKLJF:LSD
-// const { app, BrowserWindow } = require('electron')
-
-// let mainWindow = null
-
-// app.disableHardwareAcceleration();
-// app.whenReady().then(() => {
-//     app.commandLine.appendSwitch('in-process-gpu');
-//   // We cannot require the screen module until the app is ready.
-//   const { screen } = require('electron')
-
-//   // Create a window that fills the screen's available work area.
-//   const primaryDisplay = screen.getPrimaryDisplay()
-//   const { width, height } = primaryDisplay.workAreaSize
-
-//   mainWindow = new BrowserWindow({ width, height })
-//   mainWindow.loadURL('https://electronjs.org')
-// })
-
-// const { app, BrowserWindow } = require('electron')
-
-// const createWindow = () => {
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600
-//   })
-
-//   win.loadFile('index.html')
-// }
-
-// app.whenReady().then(() => {
-//   createWindow()
-// })
