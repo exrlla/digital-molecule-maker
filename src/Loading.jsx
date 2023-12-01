aae42934cd18acd8b9daf24967e5ab6fad3418db
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2023-11-30 21:12:18 -0600

    some styling and instructions

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -5,14 +5,32 @@ const Loading = () => {
     // A basic loading spinner
     return (
         <>
-            <div style={{display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "20%"}}>
+            <div style={{
+                position: 'absolute',
+                backgroundColor: 'black',
+                height: '100vh',
+                width: '100vw',
+                opacity: '0.75',
+                zIndex: '-100'
+            }}>
+
+            </div>
+            <div 
+                style={{
+                    display: 'flex',
+                    flexDirection: "column", 
+                    alignItems: "center", 
+                    justifyContent: "center", 
+                    height: '100vh',
+                }}
+            >
                 <ReactLoading
                     type={"spin"}
                     color={"#03fc4e"}
                     height={100}
                     width={100}
                 />
-                <h1 style={{fontFamily: "serif", fontVariant: "small-caps"}}>Analyzing</h1>
+                <h1 style={{fontFamily: "serif", fontVariant: "small-caps", color: 'white', opacity: '1.0'}}>Analyzing</h1>
             </div>
         </>
     )
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = ({ images, handleImageSelect, selectedImages }) => {
     //shows each available molecule to build the trimer
     return (
       <div className="sidebar" style={{backgroundColor: 'lightgray', padding: "1rem", width: "20%", color: 'black', height: '83vh'}}>
-        <h2>Select Molecules</h2>
+        <h2>Select Molecules (Click to Select)</h2>
         <div className="image-list" style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-evenly"}}>
           {/* Maps each image available to a MoleculeDisplay option */}
           {images.map((image, index) => (
