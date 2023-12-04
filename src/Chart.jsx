b940ef1f0c3b14ef01294d5deff21f105923ca56
Shreya Vinjamuri <shreyavinjamuri@wirelessprv-10-192-90-237.near.illinois.edu>
2023-12-04 13:38:31 -0600

    updating molecule names

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -15,14 +15,14 @@ const Chart = ({weight, light_absorption, lifespan, bandgap}) => {
                             <td className='stat'>{Math.round(weight * 100) / 100}/10</td>
                         </tr>
                         <tr>
-                            <td className='name'>Light Absorption:</td>
+                            <td className='name'>Reactivity:</td>
                             <td>
                                 <div className='bar' style={{width: light_absorption * 200 + "%"}}></div>
                             </td>
                             <td className='stat'>{Math.round(light_absorption * 100) / 100}/10</td>
                         </tr>
                         <tr>
-                            <td className='name'>Lifespan:</td>
+                            <td className='name'>Activation Energy:</td>
                             <td>
                                 <div className='bar' style={{width: lifespan * 200 + "%"}}></div>
                             </td>
